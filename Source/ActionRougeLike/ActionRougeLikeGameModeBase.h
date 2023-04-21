ca168f48048e284fe3c09c70e709525d685e5314
viinaji <viinaji@gmail.com>
2023-04-21 14:21:14 +0330

    Project Created

---
+++ Source/ActionRougeLike/ActionRougeLikeGameModeBase.h
@@ -0,0 +1,17 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "ActionRougeLikeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ACTIONROUGELIKE_API AActionRougeLikeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
